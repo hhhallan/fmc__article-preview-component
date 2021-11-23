1db9fee00a56fff76637f8e3fba43f0af66bb99f
hhhallan <allanstm76@gmail.com>
2021-11-23 16:24:08 +0100

    Mobile version 470px

--- js/app.js
+++ js/app.js
@@ -2,11 +2,16 @@ const share = document.querySelector('.card-profil-share');
 const shareList = document.querySelector('.hover-card');
 const arrow = document.querySelector('.arrow');
 
-share.addEventListener('mouseenter', () => {
-    shareList.style.display = 'flex';
-    arrow.style.display = 'block';
-})
-share.addEventListener('mouseleave', () => {
-    shareList.style.display = 'none';
-    arrow.style.display = 'none';
+
+share.addEventListener('click', () => {
+
+    let width = window.innerWidth;
+
+    if (width <= 470) {
+        shareList.classList.toggle('active');
+
+    } else {
+        shareList.classList.toggle('active');
+        arrow.classList.toggle('active');
+    }
 })
